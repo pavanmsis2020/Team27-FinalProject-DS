dd218696cf3b34c9741e2700054fda54dcf09fa6
Nathaniel Pellant <npellant@iu.edu>
2020-10-01 14:14:21 -0400

    Updated data model; updated CREATE TABLE accordingly.

--- app/data/CREATETABLE.sql
+++ app/data/CREATETABLE.sql
@@ -6,53 +6,41 @@ Professor Tom Gregory
 
 /* Source: https://www.w3schools.com/sql/sql_foreignkey.asp */
 CREATE TABLE Person(
-  personID INT,
-  firstName VARCHAR(255),
-  lastName VARCHAR(255),
-  positionID INT,
-  gender VARCHAR(255),
-  address VARCHAR(255),
-  dateOfBirth DATE,
-  workPhone INT,
-  mobilePhone INT,
-  startDate DATE,
-  endDate DATE,
-  radioNumber INT,
-  stationNumber INT,
-  email VARCHAR(255),
-  certificationID INT,
-  isActive BOOLEAN,
-  PRIMARY KEY(personID) NOT NULL,
-  FOREIGN KEY(positionID) REFERENCES Position(positionID) NOT NULL,
-  FOREIGN KEY(certificationID) REFERENCES Certification(certificationID) NOT NULL
-  /* NOTE TO NATE: DEPENDENCIES THING FROM I308 */
-  /* Add auto_increment to primary keys */
+    personID INT AUTO_INCREMENT,
+    firstName VARCHAR(255),
+    lastName VARCHAR(255),
+    positionID INT,
+    gender VARCHAR(255),
+    address VARCHAR(255),
+    dateOfBirth DATE,
+    workPhone INT,
+    mobilePhone INT,
+    startDate DATE,
+    endDate DATE,
+    radioNumber INT,
+    stationNumber INT,
+    email VARCHAR(255),
+    certificationID INT,
+    isActive BOOLEAN,
+    PRIMARY KEY(personID) NOT NULL,
+    FOREIGN KEY(positionID) REFERENCES Position(positionID) NOT NULL,
+    FOREIGN KEY(certificationID) REFERENCES Certification(certificationID) NOT NULL
+    /* NOTE TO NATE: DEPENDENCIES THING FROM I308 */
 );
 
 INSERT INTO Person(personID, firstName, lastName, positionID, gender, address, dateOfBirth, workPhone, mobilePhone, startDate, endDate, radioNumber, stationNumber, email, certificationID, isActive)
 
 CREATE TABLE Certification(
-  certificationID INT,
-  certificationAgency VARCHAR(255),
-  certificationName VARCHAR(255),
-  /* defaultExpiration is in years */
-  defaultExpiration INT,
-  PRIMARY KEY(certificationID)
-);
-
-CREATE TABLE Member(
-  memberID INT,
-  certificationID INT,
-  personID INT,
-  email VARCHAR(255),
-  password VARCHAR(255),
-  PRIMARY KEY(memberID) NOT NULL,
-  FOREIGN KEY(certificationID) REFERENCES Certification(certificationID) NOT NULL,
-  FOREIGN KEY(personID) REFERENCES Person(personID) NOT NULL
+    certificationID INT AUTO_INCREMENT,
+    certificationAgency VARCHAR(255),
+    certificationName VARCHAR(255),
+    /* defaultExpiration is in years */
+    defaultExpiration INT,
+    PRIMARY KEY(certificationID)
 );
 
 CREATE TABLE Position(
-    positionID INT,
+    positionID INT AUTO_INCREMENT,
     positionName VARCHAR(255),
     certificationID INT,
     PRIMARY KEY(positionID) NOT NULL,
@@ -60,10 +48,17 @@ CREATE TABLE Position(
 );
 
 CREATE TABLE Test(
-    testID INT,
+    testID INT AUTO_INCREMENT,
     personID INT,
     certificationID INT,
+    testDate DATE,
     PRIMARY KEY (testID) NOT NULL,
     FOREIGN KEY (personID) REFERENCES Person (personID) NOT NULL,
     FOREIGN KEY (certificationID) REFERENCES Certification (certificationID) NOT NULL
+);
+
+/* OPTIONAL */
+CREATE TABLE User(
+    email VARCHAR(255),
+    password VARCHAR(255)
 );
