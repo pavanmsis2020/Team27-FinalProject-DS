3d5d6d0528bd2edebc7c8b0a84944d504d194bf5
pavanmsis2020 <70539742+pavanmsis2020@users.noreply.github.com>
2020-11-01 01:34:31 -0500

    fixing submit on prevent code for view certifications page

--- app/public/api/records/edit_certification.php
+++ app/public/api/records/edit_certification.php
@@ -37,5 +37,8 @@ $pk = $db->lastInsertId();
 // Step 4: Output
 // Here, instead of giving output, I'm redirecting to the SELECT API,
 // just in case the data changed by entering it
-header('Content-Type: application/json');
-echo $json;
+// header('Content-Type: application/json');
+// echo $json;
+
+header('HTTP/1.1 303 See Other');
+header('Location: ../records/view_certifications.php/?certificationID=' . $_POST['certificationID'])
